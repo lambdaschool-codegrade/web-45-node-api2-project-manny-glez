131c0c1f8f81061af671e6f65989d32157c5565d
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-08-31 19:40:29 -0500

    Gets all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,20 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+const router = express.Router()
+
+//POSTS ENDPOINTS
+router.get('/', (req, res) => {
+  Post.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retreving posts',
+      });
+    });
+});
+
+module.exports = router;
