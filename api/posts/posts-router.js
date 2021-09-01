ab261592dbefa7823059aa6076719a38abae2b05
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-08-31 22:33:49 -0500

    Adds get method on a posts comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -74,12 +74,58 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
-router.put('/', (req, res) => {
-
+router.put('/:id', (req, res) => {
+  const { title, contents } = req.body
+  if (!title || !contents) {
+    res.status(400).json({
+      message: 'Please provide title and contents for the post',
+    })
+  } else {
+    Post.findById(req.params.id)
+      .then(stuff => {
+        if (!stuff) {
+          res.status(404).json({
+            message: 'The post with the specified ID does not exist',
+          })
+        } else {
+          return Post.update(req.params.id, req.body)
+        }
+      })
+      .then(data => {
+        if (data) {
+          return Post.findById(req.params.id)
+        }
+      })
+      .then(post => {
+        if (post) {
+          res.json(post)
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'The post information could not be modified',
+          err: err.message,
+        });
+      })
+  }
 })
 
-router.get('/:id/messages', (req, res) => {
-
+router.get('/:id/comments', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post) {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist',
+      })
+    } else {
+      const messages = await Post.findPostComments(req.params.id)
+      res.json(messages)
+    } 
+  } catch (err) {
+    res.status(500).json({
+      message: 'The comments information could not be retrieved',
+    });
+  }
 })
 
 module.exports = router;
